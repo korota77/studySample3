7e47099f63316ec8c6ead076c28e71fbd3c6f1a1
korota <pyonkiti_e@yahoo.co.jp>
2018-08-28 23:37:48 +0900

    なんかいろいろ

---
+++ studyProject02/studyProject02/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace studyProject02
+{
+    public partial class Form1 : Form
+    {
+        TcpClient tClient = new TcpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // delegateの紐づけ
+            tClient.OnConnectedEventAction += tClient_OnConnected;
+            tClient.OnDisconnectedEventAction += tClient_OnDisconnected;
+            tClient.OnReceiveDataAction += tClient_OnReceiveData;
+
+            // ユーザコントロールのリスト(画面のリスト)
+            // 初期表示の画面を最初のIndexとして追加
+            List<UserControl> userControlList = new List<UserControl>
+            {
+                UserControl1.sharedIncetance,
+                SubDisplay.sharedIncetance
+            };
+            // パネル初期表示の設定処理
+            initPanelSetting(userControlList);
+        }
+
+        /** 接続OKイベント **/
+        void tClient_OnConnected(EventArgs e)
+        {
+            //接続OK処理
+        }
+        void tClient_OnDisconnected(object sender, EventArgs e)
+        {
+            // 接続断処理
+        }
+        void tClient_(EventArgs e)
+        {
+            //接続OK処理
+        }
+        void tClient_OnReceiveData(object sender, string e)
+        {
+
+        }
+
+
+        /// <summary>t
+        /// パネルコントロールと各ユーザーコントロールの紐付け処理
+        /// </summary>
+        /// <param name="userControlList">紐づけを行うUserControllのリスト</param>
+        private void initPanelSetting(List<UserControl> userControlList)
+        {
+            // 本当は??のnull演算子を使いたいのになんか怒ってくるから雑魚判定する
+            if (userControlList == null)
+            {
+                Console.WriteLine("Error : userControllList is not exist");
+                return;
+            }
+
+            // 画面分初期化処理を反映
+            foreach (var result in userControlList.Select((panelItem, index) => new { panelItem,　index }))
+            {
+                // パネルへの登録
+                panel1.Controls.Add(result.panelItem);
+
+                // 最初のコントロールではない場合不可視状態とする
+                if (result.index != 0) 
+                {
+                    result.panelItem.Visible = false;
+                }
+               
+            }
+
+        }
+
+    }
+}
+
+
+
+
+
+// まだこっちのほうがスマート
+// Listの処理においてはforeach使うより早いらしい
+//for (int i = 0; i < userControlList.Count; i++)
+//{
+//    panel1.Controls.Add(userControlList[i]);
+//    if (i != 0)
+//    {
+//        userControlList[i].Visible = false;
+//    }
+//}
+
+
+//int roopCounter = 0;
+//foreach (UserControl userControlitem in userControlList)
+//{
+//    panel1.Controls.Add(userControlitem);
+//    // 初回のuserControl(メイン画面)以外の場合処理を行う
+//    if (roopCounter != 0)
+//    {
+//        // userControlのデフォルト設定はtrueのため、最初のControl(メイン画面)以外はfalseに設定
+//        userControlitem.Visible = false;
+//    }
+//    roopCounter++;
+//}
---
+++ studyProject02/studyProject02/UserControl1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace studyProject02
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 69.41177F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.58824F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 1, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 32.12435F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 67.87565F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 61F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 64F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 221F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(683, 435);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(477, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "サブ画面行くよぼたん";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SubDisplayButtonClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(477, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(182, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ダイアログだそうかなボタン";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2OnClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(477, 152);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(182, 39);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.TestMethodSample);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(686, 440);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
